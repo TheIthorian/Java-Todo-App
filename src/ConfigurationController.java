c7d4b5e733b1583a9a4a328db291b7e8e1e7fa60
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-24 11:52:40 +0100

    Save user config to file

--- src/ConfigurationController.java
+++ src/ConfigurationController.java
@@ -1,16 +1,36 @@
+import java.util.HashMap;
+import org.json.simple.JSONObject;
+
 public class ConfigurationController implements IConfigurationController {
+
+    private String username = null;
+    private String password = null;
+    private String configurationFilePath = "config.json";
 
     public ConfigurationController() {}
 
     public void setUsername(String username) {
-        if (username != null) {
-            System.out.println("Your username is: " + username);
-        }
+        System.out.println("Your username is: " + username);
+        this.username = username;
     }
 
     public void setPassword(String password) {
-        if (password != null) {
-            System.out.println("Your password is: " + password);
-        }
+        System.out.println("Your password is: " + password);
+        this.password = password;
+    }
+
+    public void saveToFile() {
+        HashMap<String, String> dataMap = new HashMap<String, String>();
+        dataMap.put("username", this.username);
+        dataMap.put("password", this.password);
+
+        JSONObject data = new JSONObject(dataMap);
+        FileHandler.writeJSON(this.configurationFilePath, data);
+    }
+
+    public void load(String pathname) {
+        JSONObject config = FileHandler.readJSON(pathname);
+        this.username = (String) config.get("username");
+        this.password = (String) config.get("password");
     }
 }
--- src/IConfigurationController.java
+++ src/IConfigurationController.java
@@ -2,4 +2,8 @@ public interface IConfigurationController {
     public void setUsername(String username);
 
     public void setPassword(String password);
+
+    public void saveToFile();
+
+    public void load(String pathname);
 }
