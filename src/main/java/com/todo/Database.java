bab0113087c60b46f0b3d06dee72e797a6408cfa
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-25 18:25:48 +0100

    Only create database if it doesn't exist already

--- src/main/java/com/todo/Database.java
+++ src/main/java/com/todo/Database.java
@@ -9,24 +9,30 @@ public class Database {
     private static String cachedDatabaseUrl = null;
 
     public static IConfigurationController configurationController = new ConfigurationController();
+    public static IFileHandler fileHandler = new FileHandler();
 
     public static class RecordNotFoundException extends Exception {};
 
     public Database() {}
 
+    public static boolean alreadyExists() {
+        return fileHandler.exists(getDatabaseUrl());
+    }
+
     public static void createTables() {
+        System.out.println("Creating database...");
         try {
             Connection conn = connect();
             User.createTable(conn);
             TodoModel.createTable(conn);
             conn.close();
         } catch (SQLException e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 
     public static Connection connect() throws SQLException {
-        return DriverManager.getConnection(getDatabaseUrl());
+        return DriverManager.getConnection("jdbc:sqlite:" + getDatabaseUrl());
     }
 
     private static String getDatabaseUrl() {
@@ -34,6 +40,7 @@ public class Database {
             return cachedDatabaseUrl;
         }
 
+        configurationController.load();
         cachedDatabaseUrl = configurationController.getDatabaseLocation() + databaseFileName;
         return cachedDatabaseUrl;
     }
