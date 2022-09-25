bcf00bf5b281b5d28cc014d8965ca34445728690
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-25 16:50:04 +0100

    Update TodoService to use TodoModel

--- src/main/java/com/todo/ITodoService.java
+++ src/main/java/com/todo/ITodoService.java
@@ -1,9 +1,9 @@
 package com.todo;
 
 public interface ITodoService {
-    public void addTodo(String title, String description);
+    public TodoModel addTodo(String title, String description, User user);
 
-    public void updateTodo(String title, String newTitle, String description);
+    public TodoModel updateTodo(String title, String newTitle, String description, User user);
 
-    public void deleteTodo(String title);
+    public void deleteTodo(String title, User user);
 }
--- src/main/java/com/todo/TodoService.java
+++ src/main/java/com/todo/TodoService.java
@@ -1,18 +1,37 @@
 package com.todo;
 
+import java.sql.SQLException;
+
 public class TodoService implements ITodoService {
-    public void addTodo(String title, String description) {
-        System.out.println("addTodo: " + title);
-        System.out.println("\t" + description);
+    public TodoModel addTodo(String title, String description, User user) {
+        TodoModel todo = new TodoModel(title, description, user);
+        try {
+            todo.insert(user.getId());
+        } catch (SQLException e) {
+            System.out.println("Unable to add todo item: " + e.getErrorCode());
+        }
+        return todo;
     }
 
-    public void updateTodo(String title, String newTitle, String description) {
-        System.out.println("updateTodo: " + title);
-        System.out.println("\t" + newTitle);
-        System.out.println("\t" + description);
+    public TodoModel updateTodo(String title, String newTitle, String newDescription, User user) {
+        TodoModel todo = null;
+        int userId = user.getId();
+
+        try {
+            todo = TodoModel.getByTitle(title, userId);
+            todo.title = newTitle == null ? todo.title : newTitle;
+            todo.description = newDescription == null ? todo.description : newDescription;
+            todo.update(userId);
+        } catch (SQLException e) {
+            System.out.println("Unable to add todo item: " + e.getErrorCode());
+        } catch (Database.RecordNotFoundException e) {
+            System.out.println("Unable to find todo item to update");
+        }
+
+        return todo;
     }
 
-    public void deleteTodo(String title) {
+    public void deleteTodo(String title, User user) {
         System.out.println("delete: " + title);
     }
 
