2146c91048f64d9ea5cdfe039950ba7df7c8af40
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-26 23:42:10 +0100

    Make static method return void

--- src/main/java/com/todo/ITodoService.java
+++ src/main/java/com/todo/ITodoService.java
@@ -1,12 +1,11 @@
 package com.todo;
 
-import com.todo.models.TodoModel;
 import com.todo.models.User;
 
 public interface ITodoService {
-    public TodoModel addTodo(String title, String description, User user);
+    public void addTodo(String title, String description, User user);
 
-    public TodoModel updateTodo(String title, String newTitle, String description, User user);
+    public void updateTodo(String title, String newTitle, String description, User user);
 
     public void deleteTodo(String title, User user);
 }
