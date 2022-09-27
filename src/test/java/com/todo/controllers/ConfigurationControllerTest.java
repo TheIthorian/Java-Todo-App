fe503cfa0132c94307dcf05a64e5a21071d0e6d8
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-27 18:54:10 +0100

    Move to controllers folder

--- src/test/java/com/todo/controllers/ConfigurationControllerTest.java
+++ src/test/java/com/todo/controllers/ConfigurationControllerTest.java
@@ -1,11 +1,13 @@
-package com.todo;
+package com.todo.controllers;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import java.util.HashMap;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+import com.todo.FileHandler;
+import com.todo.IResourceHandler;
 import org.json.JSONObject;
 import org.junit.Test;
 
@@ -13,20 +15,23 @@ public class ConfigurationControllerTest {
 
     private static final String existingUsername = "my_username";
     private static final String existingPassword = "my_password";
+    private static final String existingDatabaseLocation = "my_databaseLocation";
 
-    private static IFileHandler mockFileHandler;
+    private static IResourceHandler mockFileHandler;
 
-    private static JSONObject formatToJSON(String username, String password) {
+    private static JSONObject formatToJSON(String username, String password,
+            String databaseLocation) {
         HashMap<String, String> dataMap = new HashMap<String, String>();
         dataMap.put("username", username);
         dataMap.put("password", password);
+        dataMap.put("databaseLocation", databaseLocation);
         return new JSONObject(dataMap);
     }
 
     private static void setup() {
         mockFileHandler = Mockito.mock(FileHandler.class);
-        Mockito.when(mockFileHandler.readJSON("config.json"))
-                .thenReturn(formatToJSON(existingUsername, existingPassword));
+        Mockito.when(mockFileHandler.readJSON("config.json")).thenReturn(
+                formatToJSON(existingUsername, existingPassword, existingDatabaseLocation));
     }
 
     @Test
@@ -42,6 +47,7 @@ public class ConfigurationControllerTest {
         // Then
         assertEquals(existingUsername, configurationController.getUsername());
         assertEquals(existingPassword, configurationController.getPassword());
+        assertEquals(existingDatabaseLocation, configurationController.getDatabaseLocation());
         Mockito.verify(mockFileHandler, Mockito.times(1)).readJSON("config.json");
     }
 
@@ -60,6 +66,7 @@ public class ConfigurationControllerTest {
         // Then
         assertNull(configurationController.getUsername());
         assertNull(configurationController.getPassword());
+        assertNull(configurationController.getDatabaseLocation());
         Mockito.verify(mockFileHandler, Mockito.times(1)).readJSON("config.json");
     }
 
@@ -86,6 +93,7 @@ public class ConfigurationControllerTest {
 
         ConfigurationController configurationController =
                 new ConfigurationController(mockFileHandler);
+        configurationController.load();
 
         // When
         configurationController.setUsername(newUsername);
@@ -117,6 +125,7 @@ public class ConfigurationControllerTest {
 
         ConfigurationController configurationController =
                 new ConfigurationController(mockFileHandler);
+        configurationController.load();
 
         // When
         configurationController.setPassword(newPassword);
@@ -126,25 +135,66 @@ public class ConfigurationControllerTest {
     }
 
     @Test
+    public void setDatabaseLocation_changesDatabaseLocation() {
+        // Given
+        setup();
+        String newDatabaseLocation = "my_new_databaselocation";
+
+        ConfigurationController configurationController =
+                new ConfigurationController(mockFileHandler);
+
+        // When
+        configurationController.setDatabaseLocation(newDatabaseLocation);
+
+        // Then
+        assertEquals(newDatabaseLocation, configurationController.getDatabaseLocation());
+    }
+
+    @Test
+    public void setDatabaseLocation_doesNotChangeDatabaseLocation_ifDatabaseLocationIsNull() {
+        // Given
+        setup();
+        String newDatabaseLocation = null;
+
+        mockFileHandler = Mockito.mock(FileHandler.class);
+        Mockito.when(mockFileHandler.readJSON("config.json")).thenReturn(
+                formatToJSON(existingUsername, existingPassword, existingDatabaseLocation));
+
+        ConfigurationController configurationController =
+                new ConfigurationController(mockFileHandler);
+        configurationController.load();
+
+        // When
+        configurationController.setDatabaseLocation(newDatabaseLocation);
+
+        // Then
+        assertEquals(existingDatabaseLocation, configurationController.getDatabaseLocation());
+    }
+
+    @Test
     public void saveToFile_savesTheCurrentConfigurationToFile() {
         // Given
         setup();
         final String newUsername = "my_new_username";
         final String newPassword = "my_new_password";
 
-        // final JSONObject expectedData = formatToJSON(newUsername, newPassword);
-
         ConfigurationController configurationController =
                 new ConfigurationController(mockFileHandler);
+        configurationController.load();
 
         // When
         configurationController.setUsername(newUsername);
         configurationController.setPassword(newPassword);
-        configurationController.saveToFile();
+        configurationController.save();
 
         // Then
-        // Identical JSONObjects are not equal so cannot test against `expectedData`...
+        ArgumentCaptor<JSONObject> dataArgument = ArgumentCaptor.forClass(JSONObject.class);
         Mockito.verify(mockFileHandler, Mockito.times(1)).writeJSON(anyString(),
-                any(JSONObject.class));
+                dataArgument.capture());
+
+        JSONObject data = dataArgument.getValue();
+        assertEquals(data.get("username"), newUsername);
+        assertEquals(data.get("password"), newPassword);
+        assertEquals(data.get("databaseLocation"), existingDatabaseLocation);
     }
 }
