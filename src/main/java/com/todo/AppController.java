27f6536b16e0bf2d5759b4526ee41b520ef606e6
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-30 19:21:25 +0100

    Refactor AppController

--- src/main/java/com/todo/AppController.java
+++ src/main/java/com/todo/AppController.java
@@ -1,8 +1,11 @@
 package com.todo;
 
+import java.util.Arrays;
+import java.util.List;
 import com.todo.controllers.ConfigurationController;
 import com.todo.controllers.TodoService;
 import com.todo.controllers.UserService;
+import com.todo.models.TodoModel;
 import com.todo.models.User;
 
 // https://www.oracle.com/java/technologies/javase/javadoc-tool.html
@@ -24,6 +27,8 @@ public class AppController {
 
     public static Database database;
 
+    public TodoService todoService = new TodoService(null, null);
+
     public AppController() {}
 
     public void run(String[] args) {
@@ -38,19 +43,40 @@ public class AppController {
         inputHandler.awaitInput("Prees enter to close...");
     }
 
-    private void buildDatabase() {
+    public void buildDatabase() {
         if (!database.alreadyExists()) {
             database.createTables();
         }
     }
 
-    private void processArguments(ArgumentCollection arguments) {
-        processTodoOperation(arguments);
-        processNoOperation(arguments);
+    public void processArguments(ArgumentCollection arguments) {
+        User user = getUser(database, configurationController);
+        if (user == null) {
+            return;
+        }
+        System.out.println(user.getId() + " : " + user.username);
+
+        processTodoOperation(arguments, new TodoService(database, user));
+        processNoOperation(arguments, user);
         processConfigurationOperation(arguments);
     }
 
-    private void processConfigurationOperation(ArgumentCollection arguments) {
+    public User getUser(Database database, ConfigurationController configurationController) {
+        if (!configurationValidator.isValid(configurationController)) {
+            return null;
+        }
+        User user = UserService.getUser(database, configurationController.getUsername(),
+                configurationController.getPassword());
+
+        if (user == null || !user.authenticate(userAuthenticator)) {
+            System.out.println("Credentials not recognised.");
+            return null;
+        }
+
+        return user;
+    }
+
+    public void processConfigurationOperation(ArgumentCollection arguments) {
         if (arguments.contains("-cf")) {
             configurationController.setUsername(arguments.get("username"));
             configurationController.setPassword(arguments.get("password"));
@@ -70,36 +96,36 @@ public class AppController {
         }
     }
 
-    private void processTodoOperation(ArgumentCollection arguments) {
-        if (!arguments.contains("-a") && !arguments.contains("-u") && !arguments.contains("-d")) {
+    public void processTodoOperation(ArgumentCollection arguments, TodoService todoService) {
+        if (!arguments.contains(Arrays.asList("-a", "-u", "-d", "-g"))) {
             return;
         }
 
-        if (!configurationValidator.isValid(configurationController)) {
-            return;
-        }
-
-        User user = UserService.getUser(database, configurationController.getUsername(),
-                configurationController.getPassword());
-
-        if (user == null || !user.authenticate(userAuthenticator)) {
-            System.out.println("Credentials not recognised.");
-            return;
-        }
-
-        TodoService todoService = new TodoService(database);
+        System.out.println(arguments.get("-g"));
 
         if (arguments.contains("-a")) {
-            todoService.addTodo(arguments.get("title"), arguments.get("description"), user);
+            todoService.addTodo(arguments.get("title"), arguments.get("description"));
         } else if (arguments.contains("-u")) {
             todoService.updateTodo(arguments.get("title"), arguments.get("newTitle"),
-                    arguments.get("description"), user);
+                    arguments.get("description"));
         } else if (arguments.contains("-d")) {
-            todoService.deleteTodo(arguments.get("title"), user);
+            todoService.deleteTodo(arguments.get("title"));
+        } else if (arguments.contains("-g")) {
+            List<TodoModel> todos;
+
+            if (arguments.contains("title")) {
+                todos = todoService.getByTitle(arguments.get("title"));
+            } else {
+                todos = todoService.getAll();
+            }
+
+            for (TodoModel todo : todos) {
+                System.out.println(todo.getId() + "\t: " + todo.title + " : " + todo.description);
+            }
         }
     }
 
-    private void processNoOperation(ArgumentCollection arguments) {
+    private void processNoOperation(ArgumentCollection arguments, User user) {
         if (!arguments.isEmpty()) {
             return;
         }
