5fe7eacca590f7960de0839ed24f33181c446e40
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-30 17:22:43 +0100

    Fix interface

--- src/main/java/com/todo/ITodoService.java
+++ src/main/java/com/todo/ITodoService.java
@@ -1,11 +1,9 @@
 package com.todo;
 
-import com.todo.models.User;
-
 public interface ITodoService {
-    public void addTodo(String title, String description, User user);
+    public void addTodo(String title, String description);
 
-    public void updateTodo(String title, String newTitle, String description, User user);
+    public void updateTodo(String title, String newTitle, String description);
 
-    public void deleteTodo(String title, User user);
+    public void deleteTodo(String title);
 }
