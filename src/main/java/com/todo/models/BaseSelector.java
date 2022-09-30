9233661664b5dd5c263960555b42fe8929c08e58
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-30 17:22:55 +0100

    Add a base selector class

---
+++ src/main/java/com/todo/models/BaseSelector.java
@@ -0,0 +1,18 @@
+package com.todo.models;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.todo.Database;
+
+public class BaseSelector {
+    public Connection conn;
+
+    public void connect(Database database) throws SQLException {
+        this.conn = database.connect();
+    }
+
+    public void disconnect() throws SQLException {
+        this.conn.close();
+        this.conn = null;
+    }
+}
