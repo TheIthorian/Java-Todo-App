5ad7a43c1cc9f8ae913e018bc325a5da52a53fb9
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-15 12:48:36 +0100

    Make AbstractDatabase class

---
+++ src/main/java/com/todo/AbstractDatabase.java
@@ -0,0 +1,31 @@
+package com.todo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import com.todo.models.TodoModel;
+import com.todo.models.User;
+
+public abstract class AbstractDatabase {
+    public void createTables() {
+        System.out.println("Creating database...");
+        try {
+            Connection conn = connect();
+            User.createTable(conn);
+            TodoModel.createTable(conn);
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dml(String sql, Connection conn) throws SQLException {
+        System.out.println(sql);
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.executeUpdate();
+        statement.close();
+    }
+
+    public abstract Connection connect() throws SQLException;
+
+}
