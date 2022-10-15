2e444a09dfb211ac4fa7ff2360f578d10b1154e0
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-15 15:35:59 +0100

    Use selector instance instead of static methods

--- src/main/java/com/todo/controllers/UserService.java
+++ src/main/java/com/todo/controllers/UserService.java
@@ -1,39 +1,58 @@
 package com.todo.controllers;
 
-import java.sql.Connection;
 import java.sql.SQLException;
+import com.todo.AbstractDatabase;
 import com.todo.Database;
 import com.todo.models.User;
 import com.todo.models.UserSelector;
 
 public class UserService {
-    static public void addUser(Database database, String username, String password) {
-        try {
-            Connection conn = database.connect();
-            UserSelector userSelector = new UserSelector(conn);
+    private AbstractDatabase database;
 
-            if (User.usernameExists(userSelector, username)) {
+    public UserSelector selector;
+
+    public UserService(AbstractDatabase database) {
+        this.database = database;
+        this.selector = new UserSelector();
+    }
+
+    public boolean isPasswordCorrect(String username, String password) throws SQLException {
+        return (selector.selectByUsernamePassword(username, password) != null);
+    }
+
+    public void addUser(Database database, String username, String password) {
+        try {
+            selector.connect(database);
+
+            if (selector.selectByUsername(username) != null) {
                 System.out.println("Username already exists.");
                 return;
             }
 
             User user = new User(username, password);
-            user.insert(userSelector);
-            conn.close();
+            selector.insert(user);
+
+            selector.disconnect();
+
         } catch (SQLException e) {
             System.out.print("Unable to add new user:");
             e.printStackTrace();
         }
     }
 
-    static public User getUser(Database database, String username, String password) {
+    public User getUser(Database database, String username, String password) {
         System.out.println("getUser: " + username + " " + password);
         try {
-            Connection conn = database.connect();
-            UserSelector userSelector = new UserSelector(conn);
-            User user = User.getByUsernamePassword(userSelector, username, password);
-            conn.close();
-            return user;
+            selector.connect(database);
+            UserSelector.UserDto user = selector.selectByUsernamePassword(username, password);
+            selector.disconnect();
+
+            if (user != null) {
+                return new User(user);
+            }
+
+            return null;
+
         } catch (SQLException e) {
             System.out.print("Unable to get user " + username);
             e.printStackTrace();
