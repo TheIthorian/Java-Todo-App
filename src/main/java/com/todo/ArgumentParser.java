47aa370acf3f292ebebeeee70e74b9e16402c787
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-15 16:01:23 +0100

    Add doc comments

--- src/main/java/com/todo/ArgumentParser.java
+++ src/main/java/com/todo/ArgumentParser.java
@@ -1,8 +1,14 @@
 package com.todo;
 
+/*
+ * Class to parse execution arguments.
+ */
 public class ArgumentParser {
     public ArgumentParser() {}
 
+    /**
+     * Reads the list of string arguments, returning an `ArgumentCollection` object.
+     */
     public ArgumentCollection readArgs(String[] args) {
         ArgumentCollection argumentCollection = new ArgumentCollection();
 
