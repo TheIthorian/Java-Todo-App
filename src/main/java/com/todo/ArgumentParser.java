758b8f1628812b5316de6bd58c9cf9eb6041d0be
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-15 18:16:36 +0100

    Add ability to set options on argument parser

--- src/main/java/com/todo/ArgumentParser.java
+++ src/main/java/com/todo/ArgumentParser.java
@@ -1,9 +1,24 @@
 package com.todo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Class to parse execution arguments.
  */
 public class ArgumentParser {
+    public class Argument {
+        String flag;
+        String description;
+
+        public Argument(String flag, String description) {
+            this.flag = flag;
+            this.description = description;
+        }
+    }
+
+    private ArrayList<Argument> arguments = new ArrayList<Argument>();
+
     public ArgumentParser() {}
 
     /**
@@ -23,6 +38,31 @@ public class ArgumentParser {
         }
 
         return argumentCollection;
+    }
+
+    /**
+     * Adds an option to the possible list of arguments.
+     */
+    public void addOption(String flag, String description) {
+        Argument argument = new Argument(flag, description);
+        arguments.add(argument);
+    }
+
+    /**
+     * Returns a list of all allowed options.
+     */
+    public List<Argument> getOptions() {
+        return this.arguments;
+    }
+
+    /**
+     * Prints all argument options as previously defined.
+     */
+    public void printOptions() {
+        System.out.println("-h\tprints this help message");
+        for (Argument argument : arguments) {
+            System.out.println(argument.flag + "\t" + argument.description);
+        }
     }
 
 }
