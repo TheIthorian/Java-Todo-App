09a4629b549e426364940636f497215451631c24
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-15 17:41:57 +0100

    Add doc comments

--- src/main/java/com/todo/IUserAuthenticator.java
+++ src/main/java/com/todo/IUserAuthenticator.java
@@ -4,10 +4,15 @@ import com.todo.models.User;
 
 public interface IUserAuthenticator {
     /**
-     * Save the user session details
+     * Save the user session details.
      * 
      * @param user - The user to be authenticated
      * @return true if the user was successfully authenticated
      */
     public boolean authenticate(User user);
+
+    /**
+     * Returns a user matching the `username` and `password`. Returns `null` if no user is found.
+     */
+    public User getUser(String username, String password);
 }
