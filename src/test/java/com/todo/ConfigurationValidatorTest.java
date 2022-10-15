2adb24d62c9b8379cf3f22f80778a95c312894a3
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-15 16:59:55 +0100

    Add validator tests

--- src/test/java/com/todo/ConfigurationValidatorTest.java
+++ src/test/java/com/todo/ConfigurationValidatorTest.java
@@ -1,6 +1,10 @@
 package com.todo;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -17,6 +21,7 @@ public class ConfigurationValidatorTest {
 
     @BeforeClass
     public static void setup() {
+        mockConfigurationController.configurationFilePath = "config.json";
         Mockito.when(mockConfigurationController.getDatabaseLocation()).thenReturn(databaseLocation);
         Mockito.when(mockConfigurationController.getUsername()).thenReturn(username);
         Mockito.when(mockConfigurationController.getPassword()).thenReturn(password);
@@ -30,5 +35,59 @@ public class ConfigurationValidatorTest {
 
         // When / Then
         assertTrue(validator.isValid(mockConfigurationController));
+    }
+
+    @Test
+    public void isValid_returnsFalse_whenConfigIsMissingDatabaseLocation() {
+        // Given
+        setup();
+        Mockito.when(mockConfigurationController.getDatabaseLocation()).thenReturn(null);
+        ConfigurationValidator validator = new ConfigurationValidator();
+
+        // When / Then
+        assertFalse(validator.isValid(mockConfigurationController));
+    }
+
+    @Test
+    public void isValid_returnsFalse_whenConfigIsMissingUsername() {
+        // Given
+        setup();
+        Mockito.when(mockConfigurationController.getUsername()).thenReturn(null);
+        ConfigurationValidator validator = new ConfigurationValidator();
+
+        // When / Then
+        assertFalse(validator.isValid(mockConfigurationController));
+    }
+
+    @Test
+    public void isValid_returnsFalse_whenConfigIsMissingPassword() {
+        // Given
+        setup();
+        Mockito.when(mockConfigurationController.getPassword()).thenReturn(null);
+        ConfigurationValidator validator = new ConfigurationValidator();
+
+        // When / Then
+        assertFalse(validator.isValid(mockConfigurationController));
+    }
+
+    @Test 
+    public void getErrors_returnsAllErrors() {
+        // Given
+        Mockito.when(mockConfigurationController.getDatabaseLocation()).thenReturn(null);
+        Mockito.when(mockConfigurationController.getUsername()).thenReturn(null);
+        Mockito.when(mockConfigurationController.getPassword()).thenReturn(null);
+        ConfigurationValidator validator = new ConfigurationValidator();
+
+        final Map<String, String> expectedErrors = new HashMap<String, String>();
+        expectedErrors.put("databaseLocation", "databaseLocation is not specified in config.json");
+        expectedErrors.put("username", "username is not specified in config.json");
+        expectedErrors.put("password", "password is not specified in config.json");
+
+        // When 
+        validator.isValid(mockConfigurationController);
+        Map<String, String> errors = validator.getErrors();
+        
+        // Then
+        assertEquals(expectedErrors, errors);
     }
 }
