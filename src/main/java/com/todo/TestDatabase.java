97d96af044cb6f255a1a199e73ea6faae72e70d6
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-15 12:49:12 +0100

    Add TestDatabase for use in tests

---
+++ src/main/java/com/todo/TestDatabase.java
@@ -0,0 +1,29 @@
+package com.todo;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestDatabase extends AbstractDatabase {
+
+    private String dbName;
+
+    public TestDatabase(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public Connection connect() throws SQLException {
+        return DriverManager.getConnection("jdbc:sqlite:./assets/" + dbName);
+    }
+
+    public void destroy() {
+        File database = new File("./assets/" + dbName);
+        database.delete();
+    }
+
+    public void refresh() {
+        this.destroy();
+        this.createTables();
+    }
+}
