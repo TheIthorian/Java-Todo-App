ae585f43f852eb49be3a862e2b5672ee1f508f48
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-15 15:49:09 +0100

    Add doc comments

--- src/main/java/com/todo/models/BaseSelector.java
+++ src/main/java/com/todo/models/BaseSelector.java
@@ -4,13 +4,22 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import com.todo.AbstractDatabase;
 
+/**
+ * Abstract class for selectors. Includes `connect` and `disconnect` methods.
+ */
 public abstract class BaseSelector {
     public Connection conn;
 
+    /**
+     * Connects the selector instance to the database.
+     */
     public void connect(AbstractDatabase database) throws SQLException {
         this.conn = database.connect();
     }
 
+    /**
+     * Drops the current database connection.
+     */
     public void disconnect() throws SQLException {
         this.conn.close();
         this.conn = null;
