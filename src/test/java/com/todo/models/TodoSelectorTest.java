7eed5ed3425d922248ad5d3ad061b7c8a4c493e4
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-15 13:00:34 +0100

    Remove unused import

--- src/test/java/com/todo/models/TodoSelectorTest.java
+++ src/test/java/com/todo/models/TodoSelectorTest.java
@@ -3,7 +3,6 @@ package com.todo.models;
 import static org.junit.Assert.assertEquals;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
 import org.mockito.Mockito;
