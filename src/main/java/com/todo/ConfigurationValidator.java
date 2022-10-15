9cec8dd17f39028395c1823974f83538ec27a7fe
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-15 16:02:47 +0100

    Add doc comments

--- src/main/java/com/todo/ConfigurationValidator.java
+++ src/main/java/com/todo/ConfigurationValidator.java
@@ -3,11 +3,17 @@ package com.todo;
 import java.util.HashMap;
 import com.todo.controllers.ConfigurationController;
 
+/**
+ * Class to validate tha the local configuration is valid.
+ */
 public class ConfigurationValidator {
 
     private String NOT_DEFINED = "%s is not specified in ";
     private HashMap<String, String> errors = new HashMap<String, String>();
 
+    /**
+     * Returns `false` and prints any validation errors if there is any local misconfiguration.
+     */
     public boolean isValid(ConfigurationController configurationController) {
         if (configurationController.getDatabaseLocation() == null) {
             errors.put("databaseLocation",
