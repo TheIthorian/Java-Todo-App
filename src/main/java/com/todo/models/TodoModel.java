8e74cb61e4173a64b1df6dfdf946ec13843e2b7b
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-15 15:50:42 +0100

    Add doc comments

--- src/main/java/com/todo/models/TodoModel.java
+++ src/main/java/com/todo/models/TodoModel.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * A model of a todo item.
+ */
 public class TodoModel extends TodoSelector.TodoDto {
     public TodoModel(String title, String description, User user) {
         this.title = title;
@@ -15,6 +18,9 @@ public class TodoModel extends TodoSelector.TodoDto {
         super(todo);
     }
 
+    /**
+     * Executes a DDL statement to create the `todo` table.
+     */
     public static void createTable(Connection conn) throws SQLException {
         final String createTodo = "CREATE TABLE IF NOT EXISTS todo ("
                 + "id integer PRIMARY KEY, title text NOT NULL, description text, userId integer REFERENCES users (userId))";
