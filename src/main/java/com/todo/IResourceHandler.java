53f9f02b32ee4834a4cf1d87c0cc03da8e2f0497
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-15 16:11:28 +0100

    Add doc comments

--- src/main/java/com/todo/IResourceHandler.java
+++ src/main/java/com/todo/IResourceHandler.java
@@ -3,17 +3,39 @@ package com.todo;
 import java.util.Scanner;
 import org.json.JSONObject;
 
+/**
+ * Interface to interact with persistent resources.
+ */
 public interface IResourceHandler {
+    /**
+     * Returns `true` if a file exists with the given `pathname`.
+     */
     public boolean exists(String pathname);
 
+    /*
+     * Creates a new file at the given `pathname` location. Will not overwrite existing files.
+     */
     public void create(String pathname);
 
+    /**
+     * Writes the input text `data` to the specified `pathname` file location.
+     */
     public void writeText(String pathname, String data);
 
+    /**
+     * Writes the input json `data` to the specified `pathname` file location.
+     */
     public void writeJSON(String pathname, JSONObject data);
 
+    /**
+     * Returns a `Scanner` object of the file at the given `pathname` location.
+     */
     public Scanner readText(String pathname);
 
+    /**
+     * Returns a `JSONObject` object containing the json data of the file at the given `pathname`
+     * location.
+     */
     public JSONObject readJSON(String pathname);
 
 }
