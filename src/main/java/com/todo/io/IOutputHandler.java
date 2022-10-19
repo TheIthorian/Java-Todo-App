71e3a4db672e93058d63a11aa7403be2f3dfecb1
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-10-19 17:27:50 +0100

    Add OutputHandler

---
+++ src/main/java/com/todo/io/IOutputHandler.java
@@ -0,0 +1,9 @@
+package com.todo.io;
+
+public interface IOutputHandler {
+
+    /**
+     * Write a string to the output
+     */
+    void write(String str);
+}
---
+++ src/main/java/com/todo/io/OutputHandler.java
@@ -0,0 +1,8 @@
+package com.todo.io;
+
+public class OutputHandler implements IOutputHandler {
+
+    public void write(String str) {
+        System.out.println(str);
+    }
+}
