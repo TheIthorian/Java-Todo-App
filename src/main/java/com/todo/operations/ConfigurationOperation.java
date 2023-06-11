24958ab8b06f7624c768af51b9e75837097c1416
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-06-11 20:38:55 +0100

    refactor: add ConfigurationOperation

---
+++ src/main/java/com/todo/operations/ConfigurationOperation.java
@@ -0,0 +1,50 @@
+package com.todo.operations;
+
+import com.todo.ArgumentCollection;
+import com.todo.ConfigurationValidator;
+import com.todo.DatabaseManager;
+import com.todo.controllers.ConfigurationController;
+import com.todo.controllers.UserService;
+
+public class ConfigurationOperation {
+    private ConfigurationController configurationController;
+    private DatabaseManager databaseManager;
+
+    public ConfigurationOperation(ConfigurationController configurationController,
+            DatabaseManager databaseManager) {
+        this.configurationController = configurationController;
+        this.databaseManager = databaseManager;
+    }
+
+    public boolean process(ArgumentCollection arguments) {
+        if (arguments.contains("-cf")) {
+            configurationController.setUsername(arguments.get("username"));
+            configurationController.setPassword(arguments.get("password"));
+            configurationController.setDatabaseLocation(arguments.get("db"));
+            configurationController.save();
+            return true;
+        }
+
+        if (arguments.contains("-addUser")) {
+            // Are we able to add a new user?
+            ConfigurationValidator validator = new ConfigurationValidator();
+            if (!validator.isValid(configurationController)) {
+                return true;
+            }
+
+            UserService userService = new UserService(databaseManager.getDatabase());
+            try {
+                userService.addUser(configurationController.getUsername(),
+                        configurationController.getPassword());
+            } catch (UserService.UserValidationErrors.UsernameAlreadyExists e) {
+                System.out.println("Username already exists.");
+            } catch (UserService.UserValidationError e) {
+                System.out.println("Unhandled validation error: " + e.getMessage());
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+}
