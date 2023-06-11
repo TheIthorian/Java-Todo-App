48626fb1fbbc715ddf0642d6dabc849aca769369
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-06-11 20:39:09 +0100

    refactor: add TodoWizard

---
+++ src/main/java/com/todo/operations/TodoWizard.java
@@ -0,0 +1,12 @@
+package com.todo.operations;
+
+public class TodoWizard {
+    public void process() {
+        // Start the application.
+        // Walk through adding, updating etc
+
+        /*
+         * Select option: Find / All / Add / Update / Remove
+         */
+    }
+}
